fff600fe5125efef13b0864ab238feeda0da2bbe
Robert Seedorff <Robert.Seedorff@iteratec.com>
2021-03-19 09:33:14 +0100

    Added licence header to each file.

--- src/main/java/io/securecodebox/persistence/defectdojo/models/Finding.java
+++ src/main/java/io/securecodebox/persistence/defectdojo/models/Finding.java
@@ -1,6 +1,7 @@
 /*
- *
- *  SecureCodeBox (SCB)
+ *  secureCodeBox (SCB)
+ *  Copyright 2021 iteratec GmbH
+ *  https://www.iteratec.com
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -13,7 +14,6 @@
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
- * /
  */
 package io.securecodebox.persistence.defectdojo.models;
 
