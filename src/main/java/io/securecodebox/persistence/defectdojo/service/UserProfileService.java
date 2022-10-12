a489a8be3d4abe6e9071f3526b9eacd08c30d290
Simon Hülkenberg <simon.huelkenberg@iteratec.com>
2022-10-12 13:02:36 +0200

    Add user profile endpoint
    Signed-off-by: Simon Hülkenberg <simon.huelkenberg@iteratec.com>

---
+++ src/main/java/io/securecodebox/persistence/defectdojo/service/UserProfileService.java
@@ -0,0 +1,47 @@
+/*
+ *  secureCodeBox (SCB)
+ *  Copyright 2021 iteratec GmbH
+ *  https://www.iteratec.com
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package io.securecodebox.persistence.defectdojo.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import io.securecodebox.persistence.defectdojo.config.DefectDojoConfig;
+import io.securecodebox.persistence.defectdojo.models.DefectDojoResponse;
+import io.securecodebox.persistence.defectdojo.models.UserProfile;
+
+public class UserProfileService extends GenericDefectDojoService<UserProfile>{
+
+    public UserProfileService(DefectDojoConfig config) {
+        super(config);
+    }
+
+    @Override
+    protected String getUrlPath() {
+        return "user_profile";
+    }
+
+    @Override
+    protected Class<UserProfile> getModelClass() {
+        return UserProfile.class;
+    }
+
+    @Override
+    protected DefectDojoResponse<UserProfile> deserializeList(String response) throws JsonProcessingException {
+        return this.objectMapper.readValue(response, new TypeReference<>() {});
+    }
+    
+}
