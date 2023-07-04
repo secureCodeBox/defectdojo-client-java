3bde5a36fdd4a595112668efee7a5581a250e175
Sven Strittmatter <sven.strittmatter@iteratec.com>
2023-07-04 09:47:02 +0200

    #36 Add fixme tag for equals and hash code
    Signed-off-by: Sven Strittmatter <sven.strittmatter@iteratec.com>

--- src/main/java/io/securecodebox/persistence/defectdojo/models/DefectDojoModel.java
+++ src/main/java/io/securecodebox/persistence/defectdojo/models/DefectDojoModel.java
@@ -8,7 +8,7 @@ package io.securecodebox.persistence.defectdojo.models;
 import java.util.Map;
 import lombok.EqualsAndHashCode;
 
-@EqualsAndHashCode
+@EqualsAndHashCode // FIXME: Implement hashCode/equals in inheritance is problematic (https://www.artima.com/articles/how-to-write-an-equality-method-in-java)
 abstract public class DefectDojoModel {
   public abstract boolean equalsQueryString(Map<String, Object> queryParams);
 }
