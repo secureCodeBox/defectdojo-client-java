deb263578ec1a47c2c4645673984d479570f6ccf
Sven Strittmatter <sven.strittmatter@iteratec.com>
2023-07-05 15:52:57 +0200

    #36 Extract network side effect into interface so we can mock this out in the tests
    Signed-off-by: Sven Strittmatter <sven.strittmatter@iteratec.com>

--- src/main/java/io/securecodebox/persistence/defectdojo/service/DefaultImportScanService.java
+++ src/main/java/io/securecodebox/persistence/defectdojo/service/DefaultImportScanService.java
@@ -45,7 +45,7 @@ class DefaultImportScanService implements ImportScanService {
         new FormHttpMessageConverter(),
         new ResourceHttpMessageConverter(),
         new MappingJackson2HttpMessageConverter());
-    private final SystemPropertyFinder properties = new SystemPropertyFinder();
+    private final HttpRequester requester = new DefaultHttpRequester();
     @Getter
     private final String defectDojoUrl;
     @Getter
@@ -115,29 +115,15 @@ class DefaultImportScanService implements ImportScanService {
                 }
             };
 
-            // FIXME: Why do we add the whole byte array resiurce here as object? Is not simply the file name sufficient here? Then we could use <String, String>
+            // FIXME: Why do we add the whole byte array resource here as object? Is not simply the file name sufficient here? Then we could use <String, String>
             // We send the whole file content, so DefectDojo can parse the finding by itself.
             body.add("file", contentsAsResource);
 
             final var payload = new HttpEntity<MultiValueMap<String, Object>>(body, headers);
-            return exchangeRequest(endpoint, payload);
+            return requester.exchange(generateApiUrl(endpoint), payload);
         } catch (HttpClientErrorException e) {
             throw new DefectDojoPersistenceException("Failed to attach findings to engagement.");
         }
-    }
-
-    ImportScanResponse exchangeRequest(String endpoint, HttpEntity<?> payload) {
-        final var restTemplate = this.createRestTemplate();
-        return restTemplate.exchange(
-                generateApiUrl(endpoint),
-                HttpMethod.POST,
-                payload,
-                ImportScanResponse.class)
-            .getBody();
-    }
-
-    String generateApiUrl(final String endpoint) {
-        return String.format("%s/api/v2/%s/", getDefectDojoUrl(), endpoint);
     }
 
     /**
@@ -151,85 +137,8 @@ class DefaultImportScanService implements ImportScanService {
         return authorizationHeader;
     }
 
-    private RestTemplate createRestTemplate() {
-        final var template = new RestTemplate();
-
-        if (shouldConfigureProxySettings()) {
-            template.setRequestFactory(createRequestFactoryWithProxyAuthConfig());
-        }
-
-        template.setMessageConverters(HTTP_MESSAGE_CONVERTERS);
-
-        return template;
-    }
-
-    boolean shouldConfigureProxySettings() {
-        return properties.hasProperty(ProxyConfigNames.HTTP_PROXY_USER)
-            && properties.hasProperty(ProxyConfigNames.HTTP_PROXY_PASSWORD);
-    }
-
-    /**
-     * Configuring proxy authentication explicitly
-     *
-     * <p>
-     * This isn't done by default for spring rest templates.This method expects these four system properties (Java flag
-     * {@literal -DpropertyName}) to be set:
-     * </p>
-     * <ul>
-     *     <li>http.proxyUser</li>
-     *     <li>http.proxyPassword</li>
-     *     <li>http.proxyHost</li>
-     *     <li>http.proxyPort</li>
-     * </ul>
-     *
-     * @return never {@code null}
-     */
-    ClientHttpRequestFactory createRequestFactoryWithProxyAuthConfig() {
-        if (properties.notHasProperty(ProxyConfigNames.HTTP_PROXY_USER)) {
-            throw new MissingProxyAuthenticationConfig(ProxyConfigNames.HTTP_PROXY_USER);
-        }
-
-        if (properties.notHasProperty(ProxyConfigNames.HTTP_PROXY_PASSWORD)) {
-            throw new MissingProxyAuthenticationConfig(ProxyConfigNames.HTTP_PROXY_PASSWORD);
-        }
-
-        if (properties.notHasProperty(ProxyConfigNames.HTTP_PROXY_HOST)) {
-            throw new MissingProxyAuthenticationConfig(ProxyConfigNames.HTTP_PROXY_HOST);
-        }
-
-        if (properties.notHasProperty(ProxyConfigNames.HTTP_PROXY_PORT)) {
-            throw new MissingProxyAuthenticationConfig(ProxyConfigNames.HTTP_PROXY_PORT);
-        }
-
-        final var proxyHost = properties.getProperty(ProxyConfigNames.HTTP_PROXY_HOST);
-        final int proxyPort;
-        try {
-            proxyPort = Integer.parseInt(properties.getProperty(ProxyConfigNames.HTTP_PROXY_PORT));
-        } catch (final NumberFormatException e) {
-            throw new IllegalArgumentException(
-                String.format("Given port for proxy authentication configuration (property '%s') is not a valid number! Given value wa '%s'.",
-                    ProxyConfigNames.HTTP_PROXY_PORT.getLiterat(),
-                    System.getProperty("http.proxyPort")),
-                e);
-        }
-
-        final var credentials = new BasicCredentialsProvider();
-        credentials.setCredentials(
-            new AuthScope(proxyHost, proxyPort),
-            new UsernamePasswordCredentials(
-                properties.getProperty(ProxyConfigNames.HTTP_PROXY_USER),
-                properties.getProperty(ProxyConfigNames.HTTP_PROXY_PASSWORD))
-        );
-
-        final var clientBuilder = HttpClientBuilder.create();
-        clientBuilder.useSystemProperties();
-        clientBuilder.setProxy(new HttpHost(proxyHost, proxyPort));
-        clientBuilder.setDefaultCredentialsProvider(credentials);
-        clientBuilder.setProxyAuthenticationStrategy(new ProxyAuthenticationStrategy());
-
-        final var factory = new HttpComponentsClientHttpRequestFactory();
-        factory.setHttpClient(clientBuilder.build());
-        return factory;
+    String generateApiUrl(final String endpoint) {
+        return String.format("%s/api/v2/%s/", getDefectDojoUrl(), endpoint);
     }
 
     private static class SystemPropertyFinder {
@@ -249,6 +158,112 @@ class DefaultImportScanService implements ImportScanService {
     final static class MissingProxyAuthenticationConfig extends RuntimeException {
         MissingProxyAuthenticationConfig(ProxyConfigNames name) {
             super(String.format("Expected System property '%s' not set!", name.getLiterat()));
+        }
+    }
+
+    /**
+     * This interface abstracts the network side effect done by the underlying HTTP
+     */
+    interface HttpRequester {
+        ImportScanResponse exchange(String endpoint, HttpEntity<?> payload);
+    }
+
+    /**
+     * Default implementation which utilizes {@link RestTemplate}
+     */
+    static final class DefaultHttpRequester implements HttpRequester {
+        private final SystemPropertyFinder properties = new SystemPropertyFinder();
+
+        @Override
+        public ImportScanResponse exchange(final String url, final HttpEntity<?> payload) {
+            final var restTemplate = this.createRestTemplate();
+            return restTemplate.exchange(
+                    url,
+                    HttpMethod.POST,
+                    payload,
+                    ImportScanResponse.class)
+                .getBody();
+        }
+
+        private RestTemplate createRestTemplate() {
+            final var template = new RestTemplate();
+
+            if (shouldConfigureProxySettings()) {
+                template.setRequestFactory(createRequestFactoryWithProxyAuthConfig());
+            }
+
+            template.setMessageConverters(HTTP_MESSAGE_CONVERTERS);
+
+            return template;
+        }
+
+        boolean shouldConfigureProxySettings() {
+            return properties.hasProperty(ProxyConfigNames.HTTP_PROXY_USER)
+                && properties.hasProperty(ProxyConfigNames.HTTP_PROXY_PASSWORD);
+        }
+
+        /**
+         * Configuring proxy authentication explicitly
+         *
+         * <p>
+         * This isn't done by default for spring rest templates.This method expects these four system properties (Java flag
+         * {@literal -DpropertyName}) to be set:
+         * </p>
+         * <ul>
+         *     <li>http.proxyUser</li>
+         *     <li>http.proxyPassword</li>
+         *     <li>http.proxyHost</li>
+         *     <li>http.proxyPort</li>
+         * </ul>
+         *
+         * @return never {@code null}
+         */
+        ClientHttpRequestFactory createRequestFactoryWithProxyAuthConfig() {
+            if (properties.notHasProperty(ProxyConfigNames.HTTP_PROXY_USER)) {
+                throw new MissingProxyAuthenticationConfig(ProxyConfigNames.HTTP_PROXY_USER);
+            }
+
+            if (properties.notHasProperty(ProxyConfigNames.HTTP_PROXY_PASSWORD)) {
+                throw new MissingProxyAuthenticationConfig(ProxyConfigNames.HTTP_PROXY_PASSWORD);
+            }
+
+            if (properties.notHasProperty(ProxyConfigNames.HTTP_PROXY_HOST)) {
+                throw new MissingProxyAuthenticationConfig(ProxyConfigNames.HTTP_PROXY_HOST);
+            }
+
+            if (properties.notHasProperty(ProxyConfigNames.HTTP_PROXY_PORT)) {
+                throw new MissingProxyAuthenticationConfig(ProxyConfigNames.HTTP_PROXY_PORT);
+            }
+
+            final var proxyHost = properties.getProperty(ProxyConfigNames.HTTP_PROXY_HOST);
+            final int proxyPort;
+            try {
+                proxyPort = Integer.parseInt(properties.getProperty(ProxyConfigNames.HTTP_PROXY_PORT));
+            } catch (final NumberFormatException e) {
+                throw new IllegalArgumentException(
+                    String.format("Given port for proxy authentication configuration (property '%s') is not a valid number! Given value wa '%s'.",
+                        ProxyConfigNames.HTTP_PROXY_PORT.getLiterat(),
+                        System.getProperty("http.proxyPort")),
+                    e);
+            }
+
+            final var credentials = new BasicCredentialsProvider();
+            credentials.setCredentials(
+                new AuthScope(proxyHost, proxyPort),
+                new UsernamePasswordCredentials(
+                    properties.getProperty(ProxyConfigNames.HTTP_PROXY_USER),
+                    properties.getProperty(ProxyConfigNames.HTTP_PROXY_PASSWORD))
+            );
+
+            final var clientBuilder = HttpClientBuilder.create();
+            clientBuilder.useSystemProperties();
+            clientBuilder.setProxy(new HttpHost(proxyHost, proxyPort));
+            clientBuilder.setDefaultCredentialsProvider(credentials);
+            clientBuilder.setProxyAuthenticationStrategy(new ProxyAuthenticationStrategy());
+
+            final var factory = new HttpComponentsClientHttpRequestFactory();
+            factory.setHttpClient(clientBuilder.build());
+            return factory;
         }
     }
 }
--- src/test/java/io/securecodebox/persistence/defectdojo/service/DefaultImportScanServiceTest.java
+++ src/test/java/io/securecodebox/persistence/defectdojo/service/DefaultImportScanServiceTest.java
@@ -1,12 +1,16 @@
 package io.securecodebox.persistence.defectdojo.service;
 
+import io.securecodebox.persistence.defectdojo.ScanType;
 import io.securecodebox.persistence.defectdojo.config.DefectDojoConfig;
+import io.securecodebox.persistence.defectdojo.models.ScanFile;
 import io.securecodebox.persistence.defectdojo.service.DefaultImportScanService.MissingProxyAuthenticationConfig;
 import io.securecodebox.persistence.defectdojo.service.ImportScanService.ProxyConfigNames;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpHeaders;
+import org.springframework.util.LinkedMultiValueMap;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -14,6 +18,7 @@ import java.util.Map;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 /**
  * Tests for {@link DefaultImportScanService}
@@ -27,32 +32,6 @@ class DefaultImportScanServiceTest {
         42L
     );
     private final DefaultImportScanService sut = new DefaultImportScanService(config);
-    /**
-     * Since System.getProperty() is an side effect we need to back up and restore it to isolate test cases.
-     */
-    private final Map<ProxyConfigNames, String> backup = new HashMap<>();
-
-    @BeforeEach
-    void backupSystemProperties() {
-        backup.clear();
-        backup.put(ProxyConfigNames.HTTP_PROXY_HOST, System.getProperty(ProxyConfigNames.HTTP_PROXY_HOST.getLiterat()));
-        backup.put(ProxyConfigNames.HTTP_PROXY_PORT, System.getProperty(ProxyConfigNames.HTTP_PROXY_PORT.getLiterat()));
-        backup.put(ProxyConfigNames.HTTP_PROXY_USER, System.getProperty(ProxyConfigNames.HTTP_PROXY_USER.getLiterat()));
-        backup.put(ProxyConfigNames.HTTP_PROXY_PASSWORD, System.getProperty(ProxyConfigNames.HTTP_PROXY_PASSWORD.getLiterat()));
-    }
-
-    @AfterEach
-    void restoreSystemProperties() {
-        for (final var entry : backup.entrySet()) {
-            final var name = entry.getKey().getLiterat();
-
-            if (null == entry.getValue()) {
-                System.clearProperty(name);
-            } else {
-                System.setProperty(name, entry.getValue());
-            }
-        }
-    }
 
     @Test
     void constructorShouldThrowExceptionOnNullConfig() {
@@ -62,8 +41,9 @@ class DefaultImportScanServiceTest {
     }
 
     @Test
-    void createDefectDojoAuthorizationHeaders_apiKeyFromConfigShouldBePresentAsAuthHEader() {
+    void createDefectDojoAuthorizationHeaders_apiKeyFromConfigShouldBePresentAsAuthHeader() {
         final var authorizationHeaders = sut.createDefectDojoAuthorizationHeaders();
+
         assertAll(
             () -> assertThat(authorizationHeaders.size(), is(1)),
             () -> assertThat(authorizationHeaders.get(HttpHeaders.AUTHORIZATION).get(0), is("Token apiKey"))
@@ -71,111 +51,43 @@ class DefaultImportScanServiceTest {
     }
 
     @Test
-    void shouldConfigureProxySettings_falseIfNeitherUserNorPasswordIsSet() {
-        System.clearProperty(ProxyConfigNames.HTTP_PROXY_USER.getLiterat());
-        System.clearProperty(ProxyConfigNames.HTTP_PROXY_PASSWORD.getLiterat());
-
-        assertThat(sut.shouldConfigureProxySettings(), is(false));
-    }
-
-    @Test
-    void shouldConfigureProxySettings_falseIfUserSetButPasswordNot() {
-        System.setProperty(ProxyConfigNames.HTTP_PROXY_USER.getLiterat(), "user");
-        System.clearProperty(ProxyConfigNames.HTTP_PROXY_PASSWORD.getLiterat());
-
-        assertThat(sut.shouldConfigureProxySettings(), is(false));
-    }
-
-    @Test
-    void shouldConfigureProxySettings_falseIfPasswordSetButUserNot() {
-        System.clearProperty(ProxyConfigNames.HTTP_PROXY_USER.getLiterat());
-        System.setProperty(ProxyConfigNames.HTTP_PROXY_PASSWORD.getLiterat(), "password");
-
-        assertThat(sut.shouldConfigureProxySettings(), is(false));
-    }
-
-    @Test
-    void shouldConfigureProxySettings_trueIfUserAndPasswordSet() {
-        System.setProperty(ProxyConfigNames.HTTP_PROXY_USER.getLiterat(), "user");
-        System.setProperty(ProxyConfigNames.HTTP_PROXY_PASSWORD.getLiterat(), "password");
-
-        assertThat(sut.shouldConfigureProxySettings(), is(true));
-    }
-
-    @Test
-    void createRequestFactoryWithProxyAuthConfig_throesExceptionIfUserNotSet() {
-        System.clearProperty(ProxyConfigNames.HTTP_PROXY_USER.getLiterat());
-        System.setProperty(ProxyConfigNames.HTTP_PROXY_PASSWORD.getLiterat(), "password");
-        System.setProperty(ProxyConfigNames.HTTP_PROXY_HOST.getLiterat(), "host");
-        System.setProperty(ProxyConfigNames.HTTP_PROXY_PORT.getLiterat(), "4242");
-
-        final var thrown = assertThrows(
-            MissingProxyAuthenticationConfig.class,
-            sut::createRequestFactoryWithProxyAuthConfig);
-
-        assertThat(thrown.getMessage(), is("Expected System property 'http.proxyUser' not set!"));
-    }
-
-    @Test
-    void createRequestFactoryWithProxyAuthConfig_throesExceptionIfPasswordNotSet() {
-        System.setProperty(ProxyConfigNames.HTTP_PROXY_USER.getLiterat(), "user");
-        System.clearProperty(ProxyConfigNames.HTTP_PROXY_PASSWORD.getLiterat());
-        System.setProperty(ProxyConfigNames.HTTP_PROXY_HOST.getLiterat(), "host");
-        System.setProperty(ProxyConfigNames.HTTP_PROXY_PORT.getLiterat(), "4242");
-
-        final var thrown = assertThrows(
-            MissingProxyAuthenticationConfig.class,
-            sut::createRequestFactoryWithProxyAuthConfig);
-
-        assertThat(thrown.getMessage(), is("Expected System property 'http.proxyPassword' not set!"));
-    }
-
-    @Test
-    void createRequestFactoryWithProxyAuthConfig_throesExceptionIfHostNotSet() {
-        System.setProperty(ProxyConfigNames.HTTP_PROXY_USER.getLiterat(), "user");
-        System.setProperty(ProxyConfigNames.HTTP_PROXY_PASSWORD.getLiterat(), "password");
-        System.clearProperty(ProxyConfigNames.HTTP_PROXY_HOST.getLiterat());
-        System.setProperty(ProxyConfigNames.HTTP_PROXY_PORT.getLiterat(), "4242");
-
-        final var thrown = assertThrows(
-            MissingProxyAuthenticationConfig.class,
-            sut::createRequestFactoryWithProxyAuthConfig);
-
-        assertThat(thrown.getMessage(), is("Expected System property 'http.proxyHost' not set!"));
-    }
-
-    @Test
-    void createRequestFactoryWithProxyAuthConfig_throesExceptionIfPortNotSet() {
-        System.setProperty(ProxyConfigNames.HTTP_PROXY_USER.getLiterat(), "user");
-        System.setProperty(ProxyConfigNames.HTTP_PROXY_PASSWORD.getLiterat(), "password");
-        System.setProperty(ProxyConfigNames.HTTP_PROXY_HOST.getLiterat(), "host");
-        System.clearProperty(ProxyConfigNames.HTTP_PROXY_PORT.getLiterat());
-
-        final var thrown = assertThrows(
-            MissingProxyAuthenticationConfig.class,
-            sut::createRequestFactoryWithProxyAuthConfig);
-
-        assertThat(thrown.getMessage(), is("Expected System property 'http.proxyPort' not set!"));
-    }
-
-    @Test
-    void createRequestFactoryWithProxyAuthConfig_throesExceptionIfPortIsNotInteger() {
-        System.setProperty(ProxyConfigNames.HTTP_PROXY_USER.getLiterat(), "user");
-        System.setProperty(ProxyConfigNames.HTTP_PROXY_PASSWORD.getLiterat(), "password");
-        System.setProperty(ProxyConfigNames.HTTP_PROXY_HOST.getLiterat(), "host");
-        System.setProperty(ProxyConfigNames.HTTP_PROXY_PORT.getLiterat(), "FUBAR");
-
-        final var thrown = assertThrows(
-            IllegalArgumentException.class,
-            sut::createRequestFactoryWithProxyAuthConfig);
-
-        assertThat(
-            thrown.getMessage(),
-            is("Given port for proxy authentication configuration (property 'http.proxyPort') is not a valid number! Given value wa 'FUBAR'."));
-    }
-
-    @Test
     void generateApiUrl() {
         assertThat(sut.generateApiUrl("foo"), is("http://localhost/api/v2/foo/"));
+    }
+
+    @Test
+    @Disabled("Fails due to network side effect")
+    void importScan_withoutOptionsShouldPassEmptyMap() {
+        final var spiedSut = spy(sut);
+
+        sut.importScan(new ScanFile("content"),42L, 43L, "1.1.2023", ScanType.AUDIT_JS_SCAN, 23L);
+
+        verify(spiedSut, times(1))
+            .importScan(new ScanFile("content"),42L, 43L, "1.1.2023", ScanType.AUDIT_JS_SCAN, 23L, new HashMap<>());
+    }
+
+    @Test
+    @Disabled("Not implemented yet")
+    void importScan_shouldPassImportScanAsEndpoint() {
+    }
+
+    @Test
+    @Disabled("Not implemented yet")
+    void importScan_shouldPassEngagementIdAsEngagement() {
+    }
+
+    @Test
+    @Disabled("Not implemented yet")
+    void reimportScan_withoutOptionsShouldPassEmptyMap() {
+    }
+
+    @Test
+    @Disabled("Not implemented yet")
+    void reimportScan_shouldPassReimportScanAsEndpoint() {
+    }
+
+    @Test
+    @Disabled("Not implemented yet")
+    void reimportScan_shouldPassEngagementIdAsTest() {
     }
 }
