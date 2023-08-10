6a88525068c3a00d605ec817d17a0b462ce96d0c
Sven Strittmatter <sven.strittmatter@iteratec.com>
2023-08-10 20:21:31 +0200

    #51 Reformatting
    Signed-off-by: Sven Strittmatter <sven.strittmatter@iteratec.com>

--- src/test/java/io/securecodebox/persistence/defectdojo/service/UserProfileServiceTest.java
+++ src/test/java/io/securecodebox/persistence/defectdojo/service/UserProfileServiceTest.java
@@ -13,10 +13,12 @@ import java.net.URISyntaxException;
 import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.assertIterableEquals;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.client.MockRestServiceServer;
+
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
 import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
 
@@ -24,45 +26,45 @@ import static org.springframework.test.web.client.response.MockRestResponseCreat
 // This test is special because the defectdojo api does not return a list, 
 // but the generic code assumes every endpoint returns a list
 public class UserProfileServiceTest {
-    
+
     Config config;
     UserProfileService underTest;
     MockRestServiceServer mockServer;
-    
+
     // This string does not contain every field of the api response as those are not implemented
     String apiResponse = """
-                         {
-                           "user": {
-                             "id": 0,
-                             "username": "GdqmXprK.j7R+OYE49SzL3mM2U6I0DyLRHnDg87i9It0AfP-kxvswW3qOI2i+31-@0",
-                             "first_name": "string",
-                             "last_name": "string",
-                             "email": "user@example.com",
-                             "last_login": "2022-11-01T16:20:19.373Z",
-                             "is_active": true,
-                             "is_superuser": true,
-                             "configuration_permissions": [0]
-                           }
-                         }
-                         """;
-    
+        {
+          "user": {
+            "id": 0,
+            "username": "GdqmXprK.j7R+OYE49SzL3mM2U6I0DyLRHnDg87i9It0AfP-kxvswW3qOI2i+31-@0",
+            "first_name": "string",
+            "last_name": "string",
+            "email": "user@example.com",
+            "last_login": "2022-11-01T16:20:19.373Z",
+            "is_active": true,
+            "is_superuser": true,
+            "configuration_permissions": [0]
+          }
+        }
+        """;
+
     @BeforeEach
     void setup() {
         config = new Config("https://defectdojo.example.com", "abc", 42);
         underTest = new UserProfileService(config);
         mockServer = MockRestServiceServer.createServer(underTest.getRestTemplate());
     }
-    
+
     @Test
     void testSearch() throws JsonProcessingException, URISyntaxException {
-        var url = config.getUrl() + "/api/v2/" + underTest.getUrlPath() + "/?offset=0&limit=100";        
+        var url = config.getUrl() + "/api/v2/" + underTest.getUrlPath() + "/?offset=0&limit=100";
         mockServer.expect(requestTo(url)).andRespond(withSuccess(apiResponse, MediaType.APPLICATION_JSON));
-        
+
         var user = new User(0L, "GdqmXprK.j7R+OYE49SzL3mM2U6I0DyLRHnDg87i9It0AfP-kxvswW3qOI2i+31-@0", "string", "string");
         var userProfile = new UserProfile(user);
         var expected = Arrays.asList(userProfile);
         var actual = underTest.search();
-        
+
         mockServer.verify();
         assertIterableEquals(expected, actual);
     }
