83caaa8adf881738dd7b22ece70587e65ede141e
Sven Strittmatter <sven.strittmatter@iteratec.com>
2023-09-01 16:34:45 +0200

    #36 Make the default name a constant
    Signed-off-by: Sven Strittmatter <sven.strittmatter@iteratec.com>

--- src/main/java/io/securecodebox/persistence/defectdojo/model/ScanFile.java
+++ src/main/java/io/securecodebox/persistence/defectdojo/model/ScanFile.java
@@ -8,18 +8,24 @@ import lombok.Data;
 
 @Data
 public class ScanFile {
+    /**
+     * A default name must be set
+     * <p>
+     * It does not matter however unless the parser pays attention to file endings like json or xml.
+     * </p>
+     */
+    static final String DEFAULT_NAME = "default-name.txt";
 
     String content;
 
-    // a default name must be set, it does not matter however
-    // unless the parser pays attention to file endings like json or xml
-    String name = "default-name.txt";
+    String name;
 
-    public ScanFile(String content){
-        this.content = content;
+    public ScanFile(String content) {
+        this(content, DEFAULT_NAME);
     }
 
-    public ScanFile(String content, String name){
+    public ScanFile(String content, String name) {
+        super();
         this.content = content;
         this.name = name;
     }
