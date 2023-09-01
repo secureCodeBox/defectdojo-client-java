df2dae96f557c9bd0b7e74d510e31c8e97d18c5a
Sven Strittmatter <sven.strittmatter@iteratec.com>
2023-09-01 17:40:22 +0200

    Revert "#36 Extract network side effect into interface so we can mock this out in the tests"
    This reverts commit 99b039f9e9b6573b399c0f32e8687da1af3b8283.
    
    Signed-off-by: Sven Strittmatter <sven.strittmatter@iteratec.com>

--- src/main/java/io/securecodebox/persistence/defectdojo/service/DefaultImportScanService.java
+++ src/main/java/io/securecodebox/persistence/defectdojo/service/DefaultImportScanService.java
@@ -44,7 +44,7 @@ class DefaultImportScanService implements ImportScanService {
         new FormHttpMessageConverter(),
         new ResourceHttpMessageConverter(),
         new MappingJackson2HttpMessageConverter());
-    private final HttpRequester requester = new DefaultHttpRequester();
+    private final SystemPropertyFinder properties = new SystemPropertyFinder();
     @Getter
     private final String defectDojoUrl;
     @Getter
@@ -118,10 +118,24 @@ class DefaultImportScanService implements ImportScanService {
             body.add("file", contentsAsResource);
 
             final var payload = new HttpEntity<MultiValueMap<String, Object>>(body, headers);
-            return requester.exchange(generateApiUrl(endpoint), payload);
+            return exchangeRequest(endpoint, payload);
         } catch (HttpClientErrorException e) {
             throw new PersistenceException("Failed to attach findings to engagement.");
         }
+    }
+
+    ImportScanResponse exchangeRequest(String endpoint, HttpEntity<?> payload) {
+        final var restTemplate = this.createRestTemplate();
+        return restTemplate.exchange(
+                generateApiUrl(endpoint),
+                HttpMethod.POST,
+                payload,
+                ImportScanResponse.class)
+            .getBody();
+    }
+
+    String generateApiUrl(final String endpoint) {
+        return String.format("%s/api/v2/%s/", getDefectDojoUrl(), endpoint);
     }
 
     /**
@@ -135,8 +149,85 @@ class DefaultImportScanService implements ImportScanService {
         return authorizationHeader;
     }
 
-    String generateApiUrl(final String endpoint) {
-        return String.format("%s/api/v2/%s/", getDefectDojoUrl(), endpoint);
+    private RestTemplate createRestTemplate() {
+        final var template = new RestTemplate();
+
+        if (shouldConfigureProxySettings()) {
+            template.setRequestFactory(createRequestFactoryWithProxyAuthConfig());
+        }
+
+        template.setMessageConverters(HTTP_MESSAGE_CONVERTERS);
+
+        return template;
+    }
+
+    boolean shouldConfigureProxySettings() {
+        return properties.hasProperty(ProxyConfigNames.HTTP_PROXY_USER)
+            && properties.hasProperty(ProxyConfigNames.HTTP_PROXY_PASSWORD);
+    }
+
+    /**
+     * Configuring proxy authentication explicitly
+     *
+     * <p>
+     * This isn't done by default for spring rest templates.This method expects these four system properties (Java flag
+     * {@literal -DpropertyName}) to be set:
+     * </p>
+     * <ul>
+     *     <li>http.proxyUser</li>
+     *     <li>http.proxyPassword</li>
+     *     <li>http.proxyHost</li>
+     *     <li>http.proxyPort</li>
+     * </ul>
+     *
+     * @return never {@code null}
+     */
+    ClientHttpRequestFactory createRequestFactoryWithProxyAuthConfig() {
+        if (properties.notHasProperty(ProxyConfigNames.HTTP_PROXY_USER)) {
+            throw new MissingProxyAuthenticationConfig(ProxyConfigNames.HTTP_PROXY_USER);
+        }
+
+        if (properties.notHasProperty(ProxyConfigNames.HTTP_PROXY_PASSWORD)) {
+            throw new MissingProxyAuthenticationConfig(ProxyConfigNames.HTTP_PROXY_PASSWORD);
+        }
+
+        if (properties.notHasProperty(ProxyConfigNames.HTTP_PROXY_HOST)) {
+            throw new MissingProxyAuthenticationConfig(ProxyConfigNames.HTTP_PROXY_HOST);
+        }
+
+        if (properties.notHasProperty(ProxyConfigNames.HTTP_PROXY_PORT)) {
+            throw new MissingProxyAuthenticationConfig(ProxyConfigNames.HTTP_PROXY_PORT);
+        }
+
+        final var proxyHost = properties.getProperty(ProxyConfigNames.HTTP_PROXY_HOST);
+        final int proxyPort;
+        try {
+            proxyPort = Integer.parseInt(properties.getProperty(ProxyConfigNames.HTTP_PROXY_PORT));
+        } catch (final NumberFormatException e) {
+            throw new IllegalArgumentException(
+                String.format("Given port for proxy authentication configuration (property '%s') is not a valid number! Given value wa '%s'.",
+                    ProxyConfigNames.HTTP_PROXY_PORT.getLiterat(),
+                    System.getProperty("http.proxyPort")),
+                e);
+        }
+
+        final var credentials = new BasicCredentialsProvider();
+        credentials.setCredentials(
+            new AuthScope(proxyHost, proxyPort),
+            new UsernamePasswordCredentials(
+                properties.getProperty(ProxyConfigNames.HTTP_PROXY_USER),
+                properties.getProperty(ProxyConfigNames.HTTP_PROXY_PASSWORD))
+        );
+
+        final var clientBuilder = HttpClientBuilder.create();
+        clientBuilder.useSystemProperties();
+        clientBuilder.setProxy(new HttpHost(proxyHost, proxyPort));
+        clientBuilder.setDefaultCredentialsProvider(credentials);
+        clientBuilder.setProxyAuthenticationStrategy(new ProxyAuthenticationStrategy());
+
+        final var factory = new HttpComponentsClientHttpRequestFactory();
+        factory.setHttpClient(clientBuilder.build());
+        return factory;
     }
 
     private static class SystemPropertyFinder {
@@ -156,112 +247,6 @@ class DefaultImportScanService implements ImportScanService {
     final static class MissingProxyAuthenticationConfig extends RuntimeException {
         MissingProxyAuthenticationConfig(ProxyConfigNames name) {
             super(String.format("Expected System property '%s' not set!", name.getLiterat()));
-        }
-    }
-
-    /**
-     * This interface abstracts the network side effect done by the underlying HTTP
-     */
-    interface HttpRequester {
-        ImportScanResponse exchange(String endpoint, HttpEntity<?> payload);
-    }
-
-    /**
-     * Default implementation which utilizes {@link RestTemplate}
-     */
-    static final class DefaultHttpRequester implements HttpRequester {
-        private final SystemPropertyFinder properties = new SystemPropertyFinder();
-
-        @Override
-        public ImportScanResponse exchange(final String url, final HttpEntity<?> payload) {
-            final var restTemplate = this.createRestTemplate();
-            return restTemplate.exchange(
-                    url,
-                    HttpMethod.POST,
-                    payload,
-                    ImportScanResponse.class)
-                .getBody();
-        }
-
-        private RestTemplate createRestTemplate() {
-            final var template = new RestTemplate();
-
-            if (shouldConfigureProxySettings()) {
-                template.setRequestFactory(createRequestFactoryWithProxyAuthConfig());
-            }
-
-            template.setMessageConverters(HTTP_MESSAGE_CONVERTERS);
-
-            return template;
-        }
-
-        boolean shouldConfigureProxySettings() {
-            return properties.hasProperty(ProxyConfigNames.HTTP_PROXY_USER)
-                && properties.hasProperty(ProxyConfigNames.HTTP_PROXY_PASSWORD);
-        }
-
-        /**
-         * Configuring proxy authentication explicitly
-         *
-         * <p>
-         * This isn't done by default for spring rest templates.This method expects these four system properties (Java flag
-         * {@literal -DpropertyName}) to be set:
-         * </p>
-         * <ul>
-         *     <li>http.proxyUser</li>
-         *     <li>http.proxyPassword</li>
-         *     <li>http.proxyHost</li>
-         *     <li>http.proxyPort</li>
-         * </ul>
-         *
-         * @return never {@code null}
-         */
-        ClientHttpRequestFactory createRequestFactoryWithProxyAuthConfig() {
-            if (properties.notHasProperty(ProxyConfigNames.HTTP_PROXY_USER)) {
-                throw new MissingProxyAuthenticationConfig(ProxyConfigNames.HTTP_PROXY_USER);
-            }
-
-            if (properties.notHasProperty(ProxyConfigNames.HTTP_PROXY_PASSWORD)) {
-                throw new MissingProxyAuthenticationConfig(ProxyConfigNames.HTTP_PROXY_PASSWORD);
-            }
-
-            if (properties.notHasProperty(ProxyConfigNames.HTTP_PROXY_HOST)) {
-                throw new MissingProxyAuthenticationConfig(ProxyConfigNames.HTTP_PROXY_HOST);
-            }
-
-            if (properties.notHasProperty(ProxyConfigNames.HTTP_PROXY_PORT)) {
-                throw new MissingProxyAuthenticationConfig(ProxyConfigNames.HTTP_PROXY_PORT);
-            }
-
-            final var proxyHost = properties.getProperty(ProxyConfigNames.HTTP_PROXY_HOST);
-            final int proxyPort;
-            try {
-                proxyPort = Integer.parseInt(properties.getProperty(ProxyConfigNames.HTTP_PROXY_PORT));
-            } catch (final NumberFormatException e) {
-                throw new IllegalArgumentException(
-                    String.format("Given port for proxy authentication configuration (property '%s') is not a valid number! Given value wa '%s'.",
-                        ProxyConfigNames.HTTP_PROXY_PORT.getLiterat(),
-                        System.getProperty("http.proxyPort")),
-                    e);
-            }
-
-            final var credentials = new BasicCredentialsProvider();
-            credentials.setCredentials(
-                new AuthScope(proxyHost, proxyPort),
-                new UsernamePasswordCredentials(
-                    properties.getProperty(ProxyConfigNames.HTTP_PROXY_USER),
-                    properties.getProperty(ProxyConfigNames.HTTP_PROXY_PASSWORD))
-            );
-
-            final var clientBuilder = HttpClientBuilder.create();
-            clientBuilder.useSystemProperties();
-            clientBuilder.setProxy(new HttpHost(proxyHost, proxyPort));
-            clientBuilder.setDefaultCredentialsProvider(credentials);
-            clientBuilder.setProxyAuthenticationStrategy(new ProxyAuthenticationStrategy());
-
-            final var factory = new HttpComponentsClientHttpRequestFactory();
-            factory.setHttpClient(clientBuilder.build());
-            return factory;
         }
     }
 }
