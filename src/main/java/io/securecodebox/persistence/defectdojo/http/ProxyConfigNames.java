4fa891dc9d76912a38dc8e53b4fef4c778412a72
Sven Strittmatter <sven.strittmatter@iteratec.com>
2023-09-03 15:23:07 +0200

    #36 Extract config name enum in own file forbetter readability
    Signed-off-by: Sven Strittmatter <sven.strittmatter@iteratec.com>

---
+++ src/main/java/io/securecodebox/persistence/defectdojo/http/ProxyConfigNames.java
@@ -0,0 +1,39 @@
+package io.securecodebox.persistence.defectdojo.http;
+
+import lombok.Getter;
+
+/**
+ * These properties can be configured by passing them to the running Java process w/ flag {@literal -D}
+ * <p>
+ * Example: {@literal java -Dhttp.proxyHost=... -D... -jar ...}
+ * </p>
+ * <p>
+ * <strong>Important</strong>: All four parameters are mandatory. You must set them all
+ * or none of them!
+ * </p>
+ */
+@Getter
+public enum ProxyConfigNames {
+    /**
+     * System property name for the proxy username
+     */
+    HTTP_PROXY_USER("http.proxyUser"),
+    /**
+     * System property name for the proxy user's password
+     */
+    HTTP_PROXY_PASSWORD("http.proxyPassword"),
+    /**
+     * System property name for the proxy's hostname
+     */
+    HTTP_PROXY_HOST("http.proxyHost"),
+    /**
+     * System property for the proxy's port number
+     */
+    HTTP_PROXY_PORT("http.proxyPort");
+
+    private final String literat;
+
+    ProxyConfigNames(String literat) {
+        this.literat = literat;
+    }
+}
