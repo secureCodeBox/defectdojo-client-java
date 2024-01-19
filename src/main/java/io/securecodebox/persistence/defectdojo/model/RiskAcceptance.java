3269303475bc08ac46c2d8a8f64a0ec0efdcb5c8
Sven Strittmatter <sven.strittmatter@iteratec.com>
2024-01-19 23:35:44 +0100

    Simplify condition
    Signed-off-by: Sven Strittmatter <sven.strittmatter@iteratec.com>

--- src/main/java/io/securecodebox/persistence/defectdojo/model/RiskAcceptance.java
+++ src/main/java/io/securecodebox/persistence/defectdojo/model/RiskAcceptance.java
@@ -58,9 +58,6 @@ public class RiskAcceptance extends BaseModel {
 
   @Override
   public boolean equalsQueryString(Map<String, Object> queryParams) {
-    if (queryParams.containsKey("id") && queryParams.get("id").equals(this.id)) {
-      return true;
-    }
-    return false;
+      return queryParams.containsKey("id") && queryParams.get("id").equals(this.id);
   }
 }
