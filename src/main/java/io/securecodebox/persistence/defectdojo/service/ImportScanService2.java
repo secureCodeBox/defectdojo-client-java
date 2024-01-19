211fed81a7502240b0851b435b0938b829195a62
Sven Strittmatter <sven.strittmatter@iteratec.com>
2024-01-19 23:34:08 +0100

    Reformatting
    Signed-off-by: Sven Strittmatter <sven.strittmatter@iteratec.com>

--- src/main/java/io/securecodebox/persistence/defectdojo/service/ImportScanService2.java
+++ src/main/java/io/securecodebox/persistence/defectdojo/service/ImportScanService2.java
@@ -33,106 +33,106 @@ import java.util.List;
  */
 public class ImportScanService2 {
 
-    private final Config config;
+  private final Config config;
 
-    public ImportScanService2(Config config) {
-        super();
-        this.config = config;
+  public ImportScanService2(Config config) {
+    super();
+    this.config = config;
+  }
+
+  /**
+   * @return The DefectDojo Authentication Header
+   */
+  private HttpHeaders getDefectDojoAuthorizationHeaders() {
+    return new Foo(config, new ProxyConfigFactory().create()).generateAuthorizationHeaders();
+  }
+
+  protected RestTemplate setupRestTemplate() {
+    return new Foo(config, new ProxyConfigFactory().create()).createRestTemplate();
+  }
+
+  /**
+   * Before version 1.5.4. testName (in DefectDojo _test_type_) must be defectDojoScanName, afterwards, you can have somethings else
+   */
+  protected ImportScanResponse createFindings(ScanFile scanFile, String endpoint, long lead, String currentDate, ScanType scanType, long testType, MultiValueMap<String, Object> options) {
+    var restTemplate = this.setupRestTemplate();
+    HttpHeaders headers = getDefectDojoAuthorizationHeaders();
+    headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+    restTemplate.setMessageConverters(List.of(
+      new FormHttpMessageConverter(),
+      new ResourceHttpMessageConverter(),
+      new MappingJackson2HttpMessageConverter())
+    );
+
+    MultiValueMap<String, Object> mvn = new LinkedMultiValueMap<>();
+
+    mvn.add("lead", Long.toString(lead));
+    mvn.add("scan_date", currentDate);
+    mvn.add("scan_type", scanType.getTestType());
+    mvn.add("close_old_findings", "true");
+    mvn.add("skip_duplicates", "false");
+    mvn.add("test_type", String.valueOf(testType));
+
+    for (String theKey : options.keySet()) {
+      mvn.remove(theKey);
     }
+    mvn.addAll(options);
 
-    /**
-     * @return The DefectDojo Authentication Header
-     */
-    private HttpHeaders getDefectDojoAuthorizationHeaders() {
-        return new Foo(config, new ProxyConfigFactory().create()).generateAuthorizationHeaders();
-    }
-
-    protected RestTemplate setupRestTemplate() {
-        return new Foo(config, new ProxyConfigFactory().create()).createRestTemplate();
-    }
-
-    /**
-     * Before version 1.5.4. testName (in DefectDojo _test_type_) must be defectDojoScanName, afterwards, you can have somethings else
-     */
-    protected ImportScanResponse createFindings(ScanFile scanFile, String endpoint, long lead, String currentDate, ScanType scanType, long testType, MultiValueMap<String, Object> options) {
-        var restTemplate = this.setupRestTemplate();
-        HttpHeaders headers = getDefectDojoAuthorizationHeaders();
-        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
-        restTemplate.setMessageConverters(List.of(
-                new FormHttpMessageConverter(),
-                new ResourceHttpMessageConverter(),
-                new MappingJackson2HttpMessageConverter())
-        );
-
-        MultiValueMap<String, Object> mvn = new LinkedMultiValueMap<>();
-
-        mvn.add("lead", Long.toString(lead));
-        mvn.add("scan_date", currentDate);
-        mvn.add("scan_type", scanType.getTestType());
-        mvn.add("close_old_findings", "true");
-        mvn.add("skip_duplicates", "false");
-        mvn.add("test_type", String.valueOf(testType));
-
-        for (String theKey : options.keySet()) {
-            mvn.remove(theKey);
+    try {
+      ByteArrayResource contentsAsResource = new ByteArrayResource(scanFile.getContent().getBytes(StandardCharsets.UTF_8)) {
+        @Override
+        public String getFilename() {
+          return scanFile.getName();
         }
-        mvn.addAll(options);
+      };
 
-        try {
-            ByteArrayResource contentsAsResource = new ByteArrayResource(scanFile.getContent().getBytes(StandardCharsets.UTF_8)) {
-                @Override
-                public String getFilename() {
-                    return scanFile.getName();
-                }
-            };
+      mvn.add("file", contentsAsResource);
 
-            mvn.add("file", contentsAsResource);
+      var payload = new HttpEntity<>(mvn, headers);
 
-            var payload = new HttpEntity<>(mvn, headers);
-
-            return restTemplate.exchange(config.getUrl() + "/api/v2/" + endpoint + "/", HttpMethod.POST, payload, ImportScanResponse.class).getBody();
-        } catch (HttpClientErrorException e) {
-            throw new PersistenceException("Failed to attach findings to engagement.");
-        }
+      return restTemplate.exchange(config.getUrl() + "/api/v2/" + endpoint + "/", HttpMethod.POST, payload, ImportScanResponse.class).getBody();
+    } catch (HttpClientErrorException e) {
+      throw new PersistenceException("Failed to attach findings to engagement.");
     }
+  }
 
 
-    public ImportScanResponse importScan(ScanFile scanFile, long engagementId, long lead, String currentDate, ScanType scanType, long testType) {
-        var additionalValues = new LinkedMultiValueMap<String, Object>();
-        additionalValues.add("engagement", Long.toString(engagementId));
+  public ImportScanResponse importScan(ScanFile scanFile, long engagementId, long lead, String currentDate, ScanType scanType, long testType) {
+    var additionalValues = new LinkedMultiValueMap<String, Object>();
+    additionalValues.add("engagement", Long.toString(engagementId));
 
-        return this.importScan(scanFile, engagementId, lead, currentDate, scanType, testType, additionalValues);
-    }
+    return this.importScan(scanFile, engagementId, lead, currentDate, scanType, testType, additionalValues);
+  }
 
-    public ImportScanResponse reimportScan(ScanFile scanFile, long testId, long lead, String currentDate, ScanType scanType, long testType) {
-        var additionalValues = new LinkedMultiValueMap<String, Object>();
-        additionalValues.add("test", Long.toString(testId));
+  public ImportScanResponse reimportScan(ScanFile scanFile, long testId, long lead, String currentDate, ScanType scanType, long testType) {
+    var additionalValues = new LinkedMultiValueMap<String, Object>();
+    additionalValues.add("test", Long.toString(testId));
 
-        return this.reimportScan(scanFile, testId, lead, currentDate, scanType, testType, additionalValues);
-    }
+    return this.reimportScan(scanFile, testId, lead, currentDate, scanType, testType, additionalValues);
+  }
 
-    //overloading with optional parameter
-    public ImportScanResponse importScan(ScanFile scanFile, long engagementId, long lead, String currentDate, ScanType scanType, long testType, LinkedMultiValueMap<String, Object> additionalValues) {
-        additionalValues.add("engagement", Long.toString(engagementId));
+  //overloading with optional parameter
+  public ImportScanResponse importScan(ScanFile scanFile, long engagementId, long lead, String currentDate, ScanType scanType, long testType, LinkedMultiValueMap<String, Object> additionalValues) {
+    additionalValues.add("engagement", Long.toString(engagementId));
 
-        return this.createFindings(scanFile, "import-scan", lead, currentDate, scanType, testType, additionalValues);
-    }
+    return this.createFindings(scanFile, "import-scan", lead, currentDate, scanType, testType, additionalValues);
+  }
 
-    public ImportScanResponse reimportScan(ScanFile scanFile, long testId, long lead, String currentDate, ScanType scanType, long testType, LinkedMultiValueMap<String, Object> additionalValues) {
-        additionalValues.add("test", Long.toString(testId));
+  public ImportScanResponse reimportScan(ScanFile scanFile, long testId, long lead, String currentDate, ScanType scanType, long testType, LinkedMultiValueMap<String, Object> additionalValues) {
+    additionalValues.add("test", Long.toString(testId));
 
-        return this.createFindings(scanFile, "reimport-scan", lead, currentDate, scanType, testType, additionalValues);
-    }
+    return this.createFindings(scanFile, "reimport-scan", lead, currentDate, scanType, testType, additionalValues);
+  }
 
-    @Data
-    public static class ImportScanResponse {
-        @JsonProperty
-        protected Boolean verified;
+  @Data
+  public static class ImportScanResponse {
+    @JsonProperty
+    protected Boolean verified;
 
-        @JsonProperty
-        protected Boolean active;
+    @JsonProperty
+    protected Boolean active;
 
-        @JsonProperty("test")
-        protected long testId;
-    }
+    @JsonProperty("test")
+    protected long testId;
+  }
 }
