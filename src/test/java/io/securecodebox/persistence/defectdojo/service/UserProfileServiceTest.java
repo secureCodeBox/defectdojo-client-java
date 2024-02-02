fdd130caa63be656a158054c732d05aa1a70e7fd
Sven Strittmatter <sven.strittmatter@iteratec.com>
2024-02-02 14:56:27 +0100

    #23 Clean Test Code
    - Simplify by removing unnecessary setup method.
    - Use distinct values in fixture to make it easier spotting bugs.
    
    Signed-off-by: Sven Strittmatter <sven.strittmatter@iteratec.com>

--- src/test/java/io/securecodebox/persistence/defectdojo/service/UserProfileServiceTest.java
+++ src/test/java/io/securecodebox/persistence/defectdojo/service/UserProfileServiceTest.java
@@ -8,35 +8,37 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import io.securecodebox.persistence.defectdojo.config.Config;
 import io.securecodebox.persistence.defectdojo.model.User;
 import io.securecodebox.persistence.defectdojo.model.UserProfile;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.client.MockRestServiceServer;
 
 import java.net.URISyntaxException;
 import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertIterableEquals;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
 import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
 
 
-// This test is special because the defectdojo api does not return a list, 
-// but the generic code assumes every endpoint returns a list
+/**
+ * Tests for UserProfileService
+ * <p>
+ * This test is special because the defectdojo api does not return a list, but the generic code assumes every endpoint
+ * returns a list.
+ * </p>
+ */
 class UserProfileServiceTest {
-
-  private Config config;
-  private UserProfileService underTest;
-  private MockRestServiceServer mockServer;
-
-  // This string does not contain every field of the api response as those are not implemented
-  private String apiResponse = """
+  /**
+   * This string does not contain every field of the api response as those are not implemented
+   */
+  private static final String API_RESPONSE = """
     {
       "user": {
         "id": 0,
-        "username": "GdqmXprK.j7R+OYE49SzL3mM2U6I0DyLRHnDg87i9It0AfP-kxvswW3qOI2i+31-@0",
-        "first_name": "string",
-        "last_name": "string",
+        "username": "username",
+        "first_name": "first_name",
+        "last_name": "last_name",
         "email": "user@example.com",
         "last_login": "2022-11-01T16:20:19.373Z",
         "is_active": true,
@@ -46,24 +48,18 @@ class UserProfileServiceTest {
     }
     """;
 
-  @BeforeEach
-  void setup() {
-    config = new Config("https://defectdojo.example.com", "abc", 42);
-    underTest = new UserProfileService(config);
-    mockServer = MockRestServiceServer.createServer(underTest.getRestTemplate());
-  }
+  private final Config config = new Config("https://defectdojo.example.com", "abc", 42);
+  private final UserProfileService sut = new UserProfileService(config);
+  private final MockRestServiceServer server = MockRestServiceServer.createServer(sut.getRestTemplate());
 
   @Test
-  void testSearch() throws JsonProcessingException, URISyntaxException {
-    var url = config.getUrl() + "/api/v2/" + underTest.getUrlPath() + "/?offset=0&limit=100";
-    mockServer.expect(requestTo(url)).andRespond(withSuccess(apiResponse, MediaType.APPLICATION_JSON));
+  void search() throws JsonProcessingException, URISyntaxException {
+    final var url = String.format("%s/api/v2/%s/?offset=0&limit=100", config.getUrl(), sut.getUrlPath());
+    server.expect(requestTo(url)).andRespond(withSuccess(API_RESPONSE, MediaType.APPLICATION_JSON));
 
-    var user = new User(0L, "GdqmXprK.j7R+OYE49SzL3mM2U6I0DyLRHnDg87i9It0AfP-kxvswW3qOI2i+31-@0", "string", "string");
-    var userProfile = new UserProfile(user);
-    var expected = Arrays.asList(userProfile);
-    var actual = underTest.search();
+    final var expected = new UserProfile(new User(0L, "username", "first_name", "last_name"));
 
-    mockServer.verify();
-    assertIterableEquals(expected, actual);
+    assertIterableEquals(List.of(expected), sut.search());
+    server.verify();
   }
 }
