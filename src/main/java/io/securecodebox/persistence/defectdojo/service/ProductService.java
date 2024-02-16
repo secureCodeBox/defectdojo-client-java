b941ab87411b663fe87a19611024c436c7cd380f
Sven Strittmatter <sven.strittmatter@iteratec.com>
2024-02-16 15:16:19 +0100

    #121 Remove Unused Imports
    Signed-off-by: Sven Strittmatter <sven.strittmatter@iteratec.com>

--- src/main/java/io/securecodebox/persistence/defectdojo/service/ProductService.java
+++ src/main/java/io/securecodebox/persistence/defectdojo/service/ProductService.java
@@ -7,8 +7,8 @@ package io.securecodebox.persistence.defectdojo.service;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import io.securecodebox.persistence.defectdojo.config.Config;
-import io.securecodebox.persistence.defectdojo.model.Product;
 import io.securecodebox.persistence.defectdojo.model.PaginatedResult;
+import io.securecodebox.persistence.defectdojo.model.Product;
 
 public class ProductService extends GenericDefectDojoService<Product> {
 
--- src/main/java/io/securecodebox/persistence/defectdojo/service/ProductTypeService.java
+++ src/main/java/io/securecodebox/persistence/defectdojo/service/ProductTypeService.java
@@ -7,8 +7,8 @@ package io.securecodebox.persistence.defectdojo.service;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import io.securecodebox.persistence.defectdojo.config.Config;
-import io.securecodebox.persistence.defectdojo.model.ProductType;
 import io.securecodebox.persistence.defectdojo.model.PaginatedResult;
+import io.securecodebox.persistence.defectdojo.model.ProductType;
 
 public class ProductTypeService extends GenericDefectDojoService<ProductType> {
 
