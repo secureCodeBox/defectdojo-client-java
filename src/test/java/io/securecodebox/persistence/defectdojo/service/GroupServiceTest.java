c60321023fd4499ac1d3861958befa6aae5db080
Sven Strittmatter <sven.strittmatter@iteratec.com>
2024-03-08 15:29:43 +0100

    Capture & Replay Integration Test for GroupService
    Signed-off-by: Sven Strittmatter <sven.strittmatter@iteratec.com>

--- src/test/java/io/securecodebox/persistence/defectdojo/service/GroupServiceTest.java
+++ src/test/java/io/securecodebox/persistence/defectdojo/service/GroupServiceTest.java
@@ -1,14 +1,58 @@
 package io.securecodebox.persistence.defectdojo.service;
 
+import io.securecodebox.persistence.defectdojo.model.Group;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.List;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 /**
  * Tests for {@link GroupService}
  */
 final class GroupServiceTest extends WireMockBaseTestCase {
   private final GroupService sut = new GroupService(conf());
+
+  @Test
+  void search() throws URISyntaxException, IOException {
+    stubFor(
+      get("/api/v2/dojo_groups/?offset=0&limit=100")
+        .willReturn(
+          ok()
+            .withBody(readResponseBodyFromFixture("GroupService_response_fixture.json"))
+        )
+    );
+
+    final var result = sut.search();
+
+    assertAll(
+      () -> assertThat(result, hasSize(3)),
+      () -> assertThat(result, containsInAnyOrder(
+        Group.builder()
+          .id(1)
+          .name("foo")
+          .socialProvider("GitHub")
+          .users(List.of(4L))
+          .build(),
+        Group.builder()
+          .id(2)
+          .name("bar")
+          .socialProvider("GitHub")
+          .users(List.of(1L, 2L, 3L))
+          .build(),
+        Group.builder()
+          .id(3)
+          .name("snafu")
+          .socialProvider("GitHub")
+          .users(List.of(4L, 5L))
+          .build()
+      ))
+    );
+  }
 }
