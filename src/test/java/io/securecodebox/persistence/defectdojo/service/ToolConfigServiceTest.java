2a921ab9298bf8d4ab07a2670224c04c3305f94c
Sven Strittmatter <sven.strittmatter@iteratec.com>
2024-03-08 17:28:02 +0100

    Capture & Replay Integration Test for ToolConfigService
    Signed-off-by: Sven Strittmatter <sven.strittmatter@iteratec.com>

--- src/test/java/io/securecodebox/persistence/defectdojo/service/ToolConfigServiceTest.java
+++ src/test/java/io/securecodebox/persistence/defectdojo/service/ToolConfigServiceTest.java
@@ -1,14 +1,46 @@
 package io.securecodebox.persistence.defectdojo.service;
 
+import io.securecodebox.persistence.defectdojo.model.ToolConfig;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 /**
  * Tests for {@link ToolConfigService}
  */
 final class ToolConfigServiceTest extends WireMockBaseTestCase {
   private final ToolConfigService sut = new ToolConfigService(conf());
+
+  @Test
+  void search() throws URISyntaxException, IOException {
+    stubFor(
+      get("/api/v2/tool_configurations/?offset=0&limit=100")
+        .willReturn(
+          ok()
+            .withBody(readResponseBodyFromFixture("ToolConfigService_response_fixture.json"))
+        )
+    );
+
+    final var result = sut.search();
+
+    assertAll(
+      () -> assertThat(result, hasSize(1)),
+      () -> assertThat(result, containsInAnyOrder(
+        ToolConfig.builder()
+          .id(1)
+          .name("secureCodeBox")
+          .description("secureCodeBox is a kubernetes based, modularized toolchain for continuous security scans of your software project.")
+          .url("https://github.com/secureCodeBox")
+          .toolType(7)
+          .build()
+      ))
+    );
+  }
 }
