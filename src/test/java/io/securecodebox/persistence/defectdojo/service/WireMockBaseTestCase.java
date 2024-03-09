4062e917c68194a58c7cae86c2fa04afbdd67b64
Sven Strittmatter <sven.strittmatter@iteratec.com>
2024-03-09 21:11:08 +0100

    Simulate the DefectDojo Response Headers
    Signed-off-by: Sven Strittmatter <sven.strittmatter@iteratec.com>

--- src/test/java/io/securecodebox/persistence/defectdojo/service/WireMockBaseTestCase.java
+++ src/test/java/io/securecodebox/persistence/defectdojo/service/WireMockBaseTestCase.java
@@ -3,6 +3,8 @@
 // SPDX-License-Identifier: Apache-2.0
 package io.securecodebox.persistence.defectdojo.service;
 
+import com.github.tomakehurst.wiremock.http.HttpHeader;
+import com.github.tomakehurst.wiremock.http.HttpHeaders;
 import com.github.tomakehurst.wiremock.junit5.WireMockTest;
 import io.securecodebox.persistence.defectdojo.config.Config;
 import lombok.Getter;
@@ -10,6 +12,9 @@ import lombok.experimental.Accessors;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Objects;
 
 /**
@@ -33,5 +38,25 @@ abstract class WireMockBaseTestCase {
       final var bytes = Objects.requireNonNull(input).readAllBytes();
       return new String(bytes, StandardCharsets.UTF_8);
     }
+  }
+
+  HttpHeaders responseHeaders(int contentLength) {
+    return HttpHeaders.noHeaders().plus(
+      new HttpHeader("date", now()),
+      new HttpHeader("content-type", "application/json"),
+      new HttpHeader("content-length", String.valueOf(contentLength)),
+      new HttpHeader("allow", "GET, PUT, PATCH, DELETE, HEAD, OPTIONS"),
+      new HttpHeader("x-frame-options", "DENY"),
+      new HttpHeader("x-content-type-options", "nosniff"),
+      new HttpHeader("referrer-policy", "same-origin"),
+      new HttpHeader("cross-origin-opener-policy", "same-origin"),
+      new HttpHeader("vary", "Cookie"),
+      new HttpHeader("strict-transport-security", "max-age=31536000; includeSubDomains")
+    );
+  }
+
+  String now() {
+    return ZonedDateTime.now(ZoneId.of("Europe/Berlin"))
+      .format(DateTimeFormatter.RFC_1123_DATE_TIME);
   }
 }
