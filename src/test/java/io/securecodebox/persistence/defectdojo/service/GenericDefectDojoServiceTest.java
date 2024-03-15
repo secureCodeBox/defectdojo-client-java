39b322101f009600dd02486ba69b7704adee7a2e
Sven Strittmatter <sven.strittmatter@iteratec.com>
2024-03-15 20:11:05 +0100

    #121 Add Tests to Verify Presence of Proxy Auth Header
    Signed-off-by: Sven Strittmatter <sven.strittmatter@iteratec.com>

--- src/test/java/io/securecodebox/persistence/defectdojo/service/GenericDefectDojoServiceTest.java
+++ src/test/java/io/securecodebox/persistence/defectdojo/service/GenericDefectDojoServiceTest.java
@@ -1,13 +1,15 @@
 package io.securecodebox.persistence.defectdojo.service;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
-import io.securecodebox.persistence.defectdojo.exception.PersistenceException;
+import io.securecodebox.persistence.defectdojo.config.ClientConfig;
+import io.securecodebox.persistence.defectdojo.http.ProxyConfig;
 import io.securecodebox.persistence.defectdojo.model.Model;
 import io.securecodebox.persistence.defectdojo.model.PaginatedResult;
-import lombok.*;
+import lombok.NonNull;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpHeaders;
 
@@ -22,7 +24,8 @@ import static org.hamcrest.Matchers.is;
 /**
  * Tests for {@link GenericDefectDojoService}
  */
-final class GenericDefectDojoServiceTest extends WireMockBaseTestCase {
+final class GenericDefectDojoServiceTest {
+
   private static final class TestModel implements Model {
     @JsonProperty
     private long id;
@@ -37,7 +40,14 @@ final class GenericDefectDojoServiceTest extends WireMockBaseTestCase {
     }
   }
 
-  private final GenericDefectDojoService<TestModel> sut = new GenericDefectDojoService<>(conf()) {
+  private static final class TestableGenericDefectDojoService extends GenericDefectDojoService<TestModel> {
+    private TestableGenericDefectDojoService(ClientConfig clientConfig) {
+      super(clientConfig);
+    }
+
+    private TestableGenericDefectDojoService(@NonNull ClientConfig clientConfig, @NonNull ProxyConfig proxyConfig) {
+      super(clientConfig, proxyConfig);
+    }
 
     @Override
     protected String getUrlPath() {
@@ -50,15 +60,12 @@ final class GenericDefectDojoServiceTest extends WireMockBaseTestCase {
     }
 
     @Override
+    @SneakyThrows
     protected PaginatedResult<TestModel> deserializeList(@NonNull String response) {
-      try {
-        return this.objectMapper.readValue(response, new TypeReference<>() {
-        });
-      } catch (JsonProcessingException e) {
-        throw new PersistenceException("Can't process JSON response!", e);
-      }
+      return this.objectMapper.readValue(response, new TypeReference<>() {
+      });
     }
-  };
+  }
 
   private static final String JSON_SINGLE_OBJECT = """
       {"id": 42, "name": "foo"}
@@ -75,118 +82,267 @@ final class GenericDefectDojoServiceTest extends WireMockBaseTestCase {
       }
     """;
 
+  private final ClientConfig clientConfig = new ClientConfig("https://defectdojo.example.com:8080","api-key");
+  private final TestableGenericDefectDojoService sut = new TestableGenericDefectDojoService(clientConfig);
+
   @Test
   void createBaseUrl() {
-    assertThat(sut.createBaseUrl(), is(URI.create("http://localhost:8888/api/v2/snafu/")));
+    assertThat(sut.createBaseUrl(), is(URI.create("https://defectdojo.example.com:8080/api/v2/snafu/")));
   }
 
-  @Test
-  void get_containsAuthHeaderInRequest() {
-    stubFor(get(urlPathEqualTo("/api/v2/snafu/42"))
-      .willReturn(ok()
-        .withHeaders(responseHeaders(JSON_SINGLE_OBJECT.length()))
-        .withBody(JSON_SINGLE_OBJECT)));
-
-    sut.get(42L);
-
-    verify(getRequestedFor(urlPathEqualTo("/api/v2/snafu/42"))
-      .withHeader(HttpHeaders.AUTHORIZATION, equalTo("Token not-required-for-tests"))
+  @Nested
+  class AuthenticationHeaderWithoutProxyConfig extends WireMockBaseTestCase {
+    private final TestableGenericDefectDojoService sut = new TestableGenericDefectDojoService(
+      conf(), ProxyConfig.NULL
     );
+
+    @Test
+    void get_containsAuthHeaderInRequest() {
+      stubFor(get(urlPathEqualTo("/api/v2/snafu/42"))
+        .willReturn(ok()
+          .withHeaders(responseHeaders(JSON_SINGLE_OBJECT.length()))
+          .withBody(JSON_SINGLE_OBJECT)));
+
+      sut.get(42L);
+
+      verify(getRequestedFor(urlPathEqualTo("/api/v2/snafu/42"))
+        .withHeader(HttpHeaders.AUTHORIZATION, equalTo("Token " + API_KEY))
+        .withoutHeader(HttpHeaders.PROXY_AUTHORIZATION)
+      );
+    }
+
+    @Test
+    void search_containsAuthHeaderInRequest() {
+      stubFor(get(urlPathEqualTo("/api/v2/snafu/"))
+        .willReturn(ok()
+          .withHeaders(responseHeaders(JSON_MULTIPLE_OBJECT.length()))
+          .withBody(JSON_MULTIPLE_OBJECT)));
+
+      sut.search();
+
+      verify(getRequestedFor(urlPathEqualTo("/api/v2/snafu/"))
+        .withHeader(HttpHeaders.AUTHORIZATION, equalTo("Token " + API_KEY))
+        .withoutHeader(HttpHeaders.PROXY_AUTHORIZATION)
+      );
+    }
+
+    @Test
+    void searchWithQueryParams_containsAuthHeaderInRequest() {
+      stubFor(get(urlPathEqualTo("/api/v2/snafu/"))
+        .willReturn(ok()
+          .withHeaders(responseHeaders(JSON_MULTIPLE_OBJECT.length()))
+          .withBody(JSON_MULTIPLE_OBJECT)));
+
+      sut.search(Collections.emptyMap());
+
+      verify(getRequestedFor(urlPathEqualTo("/api/v2/snafu/"))
+        .withHeader(HttpHeaders.AUTHORIZATION, equalTo("Token " + API_KEY))
+        .withoutHeader(HttpHeaders.PROXY_AUTHORIZATION)
+      );
+    }
+
+    @Test
+    void searchUniqueWithObject_containsAuthHeaderInRequest() {
+      stubFor(get(urlPathEqualTo("/api/v2/snafu/"))
+        .willReturn(ok()
+          .withHeaders(responseHeaders(JSON_MULTIPLE_OBJECT.length()))
+          .withBody(JSON_MULTIPLE_OBJECT)));
+
+      sut.searchUnique(new TestModel());
+
+      verify(getRequestedFor(urlPathEqualTo("/api/v2/snafu/"))
+        .withHeader(HttpHeaders.AUTHORIZATION, equalTo("Token " + API_KEY))
+        .withoutHeader(HttpHeaders.PROXY_AUTHORIZATION)
+      );
+    }
+
+    @Test
+    void searchUniqueWithQueryParams_containsAuthHeaderInRequest() {
+      stubFor(get(urlPathEqualTo("/api/v2/snafu/"))
+        .willReturn(ok()
+          .withHeaders(responseHeaders(JSON_MULTIPLE_OBJECT.length()))
+          .withBody(JSON_MULTIPLE_OBJECT)));
+
+      sut.searchUnique(Collections.emptyMap());
+
+      verify(getRequestedFor(urlPathEqualTo("/api/v2/snafu/"))
+        .withHeader(HttpHeaders.AUTHORIZATION, equalTo("Token " + API_KEY))
+        .withoutHeader(HttpHeaders.PROXY_AUTHORIZATION)
+      );
+    }
+
+    @Test
+    void create_containsAuthHeaderInRequest() {
+      stubFor(post(urlPathEqualTo("/api/v2/snafu/"))
+        .willReturn(created()
+          .withHeaders(responseHeaders(JSON_SINGLE_OBJECT.length()))
+          .withBody(JSON_SINGLE_OBJECT)));
+
+      sut.create(new TestModel());
+
+      verify(postRequestedFor(urlPathEqualTo("/api/v2/snafu/"))
+        .withHeader(HttpHeaders.AUTHORIZATION, equalTo("Token " + API_KEY))
+        .withoutHeader(HttpHeaders.PROXY_AUTHORIZATION)
+      );
+    }
+
+    @Test
+    void delete_containsAuthHeaderInRequest() {
+      stubFor(delete(urlPathEqualTo("/api/v2/snafu/42/"))
+        .willReturn(ok()));
+
+      sut.delete(42);
+
+      verify(deleteRequestedFor(urlPathEqualTo("/api/v2/snafu/42/"))
+        .withHeader(HttpHeaders.AUTHORIZATION, equalTo("Token " + API_KEY))
+        .withoutHeader(HttpHeaders.PROXY_AUTHORIZATION)
+      );
+    }
+
+    @Test
+    void update_containsAuthHeaderInRequest() {
+      stubFor(put(urlPathEqualTo("/api/v2/snafu/42/"))
+        .willReturn(ok()
+          .withHeaders(responseHeaders(JSON_SINGLE_OBJECT.length()))
+          .withBody(JSON_SINGLE_OBJECT)));
+
+      sut.update(new TestModel(), 42);
+
+      verify(putRequestedFor(urlPathEqualTo("/api/v2/snafu/42/"))
+        .withHeader(HttpHeaders.AUTHORIZATION, equalTo("Token " + API_KEY))
+        .withoutHeader(HttpHeaders.PROXY_AUTHORIZATION)
+      );
+    }
   }
 
-  @Test
-  void search_containsAuthHeaderInRequest() {
-    stubFor(get(urlPathEqualTo("/api/v2/snafu/"))
-      .willReturn(ok()
-        .withHeaders(responseHeaders(JSON_MULTIPLE_OBJECT.length()))
-        .withBody(JSON_MULTIPLE_OBJECT)));
-
-    sut.search();
-
-    verify(getRequestedFor(urlPathEqualTo("/api/v2/snafu/"))
-      .withHeader(HttpHeaders.AUTHORIZATION, equalTo("Token not-required-for-tests"))
+  @Nested
+  class AuthenticationHeaderWithProxyConfig extends WireMockBaseTestCase {
+    private final ProxyConfig proxyConfig = ProxyConfig.builder()
+      .user("alf")
+      .password("test1234")
+      .host("proxy.owasp.org")
+      .port(8080)
+      .build();
+    private final TestableGenericDefectDojoService sut = new TestableGenericDefectDojoService(
+      conf(), proxyConfig
     );
-  }
 
-  @Test
-  void searchWithQueryParams_containsAuthHeaderInRequest() {
-    stubFor(get(urlPathEqualTo("/api/v2/snafu/"))
-      .willReturn(ok()
-        .withHeaders(responseHeaders(JSON_MULTIPLE_OBJECT.length()))
-        .withBody(JSON_MULTIPLE_OBJECT)));
+    @Test
+    void get_containsAuthHeaderInRequest() {
+      stubFor(get(urlPathEqualTo("/api/v2/snafu/42"))
+        .willReturn(ok()
+          .withHeaders(responseHeaders(JSON_SINGLE_OBJECT.length()))
+          .withBody(JSON_SINGLE_OBJECT)));
 
-    sut.search(Collections.emptyMap());
+      sut.get(42L);
 
-    verify(getRequestedFor(urlPathEqualTo("/api/v2/snafu/"))
-      .withHeader(HttpHeaders.AUTHORIZATION, equalTo("Token not-required-for-tests"))
-    );
-  }
+      verify(getRequestedFor(urlPathEqualTo("/api/v2/snafu/42"))
+        .withHeader(HttpHeaders.AUTHORIZATION, equalTo("Token " + API_KEY))
+        .withHeader(HttpHeaders.PROXY_AUTHORIZATION, equalTo("Basic YWxmOnRlc3QxMjM0"))
+      );
+    }
 
-  @Test
-  void searchUniqueWithObject_containsAuthHeaderInRequest() {
-    stubFor(get(urlPathEqualTo("/api/v2/snafu/"))
-      .willReturn(ok()
-        .withHeaders(responseHeaders(JSON_MULTIPLE_OBJECT.length()))
-        .withBody(JSON_MULTIPLE_OBJECT)));
+    @Test
+    void search_containsAuthHeaderInRequest() {
+      stubFor(get(urlPathEqualTo("/api/v2/snafu/"))
+        .willReturn(ok()
+          .withHeaders(responseHeaders(JSON_MULTIPLE_OBJECT.length()))
+          .withBody(JSON_MULTIPLE_OBJECT)));
 
-    sut.searchUnique(new TestModel());
+      sut.search();
 
-    verify(getRequestedFor(urlPathEqualTo("/api/v2/snafu/"))
-      .withHeader(HttpHeaders.AUTHORIZATION, equalTo("Token not-required-for-tests"))
-    );
-  }
+      verify(getRequestedFor(urlPathEqualTo("/api/v2/snafu/"))
+        .withHeader(HttpHeaders.AUTHORIZATION, equalTo("Token " + API_KEY))
+        .withHeader(HttpHeaders.PROXY_AUTHORIZATION, equalTo("Basic YWxmOnRlc3QxMjM0"))
+      );
+    }
 
-  @Test
-  void searchUniqueWithQueryParams_containsAuthHeaderInRequest() {
-    stubFor(get(urlPathEqualTo("/api/v2/snafu/"))
-      .willReturn(ok()
-        .withHeaders(responseHeaders(JSON_MULTIPLE_OBJECT.length()))
-        .withBody(JSON_MULTIPLE_OBJECT)));
+    @Test
+    void searchWithQueryParams_containsAuthHeaderInRequest() {
+      stubFor(get(urlPathEqualTo("/api/v2/snafu/"))
+        .willReturn(ok()
+          .withHeaders(responseHeaders(JSON_MULTIPLE_OBJECT.length()))
+          .withBody(JSON_MULTIPLE_OBJECT)));
 
-    sut.searchUnique(Collections.emptyMap());
+      sut.search(Collections.emptyMap());
 
-    verify(getRequestedFor(urlPathEqualTo("/api/v2/snafu/"))
-      .withHeader(HttpHeaders.AUTHORIZATION, equalTo("Token not-required-for-tests"))
-    );
-  }
+      verify(getRequestedFor(urlPathEqualTo("/api/v2/snafu/"))
+        .withHeader(HttpHeaders.AUTHORIZATION, equalTo("Token " + API_KEY))
+        .withHeader(HttpHeaders.PROXY_AUTHORIZATION, equalTo("Basic YWxmOnRlc3QxMjM0"))
+      );
+    }
 
-  @Test
-  void create_containsAuthHeaderInRequest() {
-    stubFor(post(urlPathEqualTo("/api/v2/snafu/"))
-      .willReturn(created()
-        .withHeaders(responseHeaders(JSON_SINGLE_OBJECT.length()))
-        .withBody(JSON_SINGLE_OBJECT)));
+    @Test
+    void searchUniqueWithObject_containsAuthHeaderInRequest() {
+      stubFor(get(urlPathEqualTo("/api/v2/snafu/"))
+        .willReturn(ok()
+          .withHeaders(responseHeaders(JSON_MULTIPLE_OBJECT.length()))
+          .withBody(JSON_MULTIPLE_OBJECT)));
 
-    sut.create(new TestModel());
+      sut.searchUnique(new TestModel());
 
-    verify(postRequestedFor(urlPathEqualTo("/api/v2/snafu/"))
-      .withHeader(HttpHeaders.AUTHORIZATION, equalTo("Token not-required-for-tests"))
-    );
-  }
+      verify(getRequestedFor(urlPathEqualTo("/api/v2/snafu/"))
+        .withHeader(HttpHeaders.AUTHORIZATION, equalTo("Token " + API_KEY))
+        .withHeader(HttpHeaders.PROXY_AUTHORIZATION, equalTo("Basic YWxmOnRlc3QxMjM0"))
+      );
+    }
 
-  @Test
-  void delete_containsAuthHeaderInRequest() {
-    stubFor(delete(urlPathEqualTo("/api/v2/snafu/42/"))
-      .willReturn(ok()));
+    @Test
+    void searchUniqueWithQueryParams_containsAuthHeaderInRequest() {
+      stubFor(get(urlPathEqualTo("/api/v2/snafu/"))
+        .willReturn(ok()
+          .withHeaders(responseHeaders(JSON_MULTIPLE_OBJECT.length()))
+          .withBody(JSON_MULTIPLE_OBJECT)));
 
-    sut.delete(42);
+      sut.searchUnique(Collections.emptyMap());
 
-    verify(deleteRequestedFor(urlPathEqualTo("/api/v2/snafu/42/"))
-      .withHeader(HttpHeaders.AUTHORIZATION, equalTo("Token not-required-for-tests"))
-    );
-  }
+      verify(getRequestedFor(urlPathEqualTo("/api/v2/snafu/"))
+        .withHeader(HttpHeaders.AUTHORIZATION, equalTo("Token " + API_KEY))
+        .withHeader(HttpHeaders.PROXY_AUTHORIZATION, equalTo("Basic YWxmOnRlc3QxMjM0"))
+      );
+    }
 
-  @Test
-  void update_containsAuthHeaderInRequest() {
-    stubFor(put(urlPathEqualTo("/api/v2/snafu/42/"))
-      .willReturn(ok()
-        .withHeaders(responseHeaders(JSON_SINGLE_OBJECT.length()))
-        .withBody(JSON_SINGLE_OBJECT)));
+    @Test
+    void create_containsAuthHeaderInRequest() {
+      stubFor(post(urlPathEqualTo("/api/v2/snafu/"))
+        .willReturn(created()
+          .withHeaders(responseHeaders(JSON_SINGLE_OBJECT.length()))
+          .withBody(JSON_SINGLE_OBJECT)));
 
-    sut.update(new TestModel(), 42);
+      sut.create(new TestModel());
 
-    verify(putRequestedFor(urlPathEqualTo("/api/v2/snafu/42/"))
-      .withHeader(HttpHeaders.AUTHORIZATION, equalTo("Token not-required-for-tests"))
-    );
+      verify(postRequestedFor(urlPathEqualTo("/api/v2/snafu/"))
+        .withHeader(HttpHeaders.AUTHORIZATION, equalTo("Token " + API_KEY))
+        .withHeader(HttpHeaders.PROXY_AUTHORIZATION, equalTo("Basic YWxmOnRlc3QxMjM0"))
+      );
+    }
+
+    @Test
+    void delete_containsAuthHeaderInRequest() {
+      stubFor(delete(urlPathEqualTo("/api/v2/snafu/42/"))
+        .willReturn(ok()));
+
+      sut.delete(42);
+
+      verify(deleteRequestedFor(urlPathEqualTo("/api/v2/snafu/42/"))
+        .withHeader(HttpHeaders.AUTHORIZATION, equalTo("Token " + API_KEY))
+        .withHeader(HttpHeaders.PROXY_AUTHORIZATION, equalTo("Basic YWxmOnRlc3QxMjM0"))
+      );
+    }
+
+    @Test
+    void update_containsAuthHeaderInRequest() {
+      stubFor(put(urlPathEqualTo("/api/v2/snafu/42/"))
+        .willReturn(ok()
+          .withHeaders(responseHeaders(JSON_SINGLE_OBJECT.length()))
+          .withBody(JSON_SINGLE_OBJECT)));
+
+      sut.update(new TestModel(), 42);
+
+      verify(putRequestedFor(urlPathEqualTo("/api/v2/snafu/42/"))
+        .withHeader(HttpHeaders.AUTHORIZATION, equalTo("Token " + API_KEY))
+        .withHeader(HttpHeaders.PROXY_AUTHORIZATION, equalTo("Basic YWxmOnRlc3QxMjM0"))
+      );
+    }
   }
 }
